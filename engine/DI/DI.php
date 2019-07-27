3453df3813eb6b51ba69a8f59215d5bbd11c5d5b
Roman Slipchenko <romaslip32@gmail.com>
2019-07-27 19:54:45 +0300

    Dependency injection Структура

---
+++ engine/DI/DI.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace Engine\DI;
+
+/**
+ * Class DI
+ * @package Engine\DI
+ */
+class DI
+{
+
+    /**
+     * @var array
+     */
+    private $container = [];
+
+    /**
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function set($key, $value)
+    {
+        $this->container[$key] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function get($key)
+    {
+        return $this->has($key);
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function has($key)
+    {
+        return isset($this->container[$key]);
+    }
+
+
+}
